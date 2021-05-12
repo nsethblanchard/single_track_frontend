40d9ff2b0a0ffe3234561d873d3244dc91adda0a
Seth Blanchard <nsethblanchard@gmail.com>
2021-05-12 14:46:32 -0400

    cleaned up js file

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const storeIndex = "http://localhost:3000/api/stores"
+const storeContainer = document.querySelector("#store-container")
 
 document.addEventListener("DOMContentLoaded", function(e){
     e.preventDefault()
     getStoresIndex()
-
-
 });
 
 function getStoresIndex(){
     fetch(storeIndex)
     .then (resp => resp.json())
     .then (stores => {
-        stores.data.forEach(store => {
+        stores.data.map(store => {
             const showStore = `
                 <div id=${store.id}>
                     <h1>Store Name: ${store.attributes.name}</h1>
@@ -22,48 +21,25 @@ function getStoresIndex(){
                     <br>
                     <br>
                     <br>
-                </div>
-            `
+                </div>`
         document.querySelector('#store-container').innerHTML += showStore;    
         })
     })
-
-    const storeContainer = document.querySelector("#store-container")
-
-    storeContainer.addEventListener('click', function(e){
-        fetch(`http://localhost:3000/api/stores/${e.target.id}`)
-        .then(resp => resp.json())
-        .then(info => {
-            // console.log(info.data.attributes.customers.length)
-
-            info.data.attributes.customers.forEach(cust =>{
-                console.log(cust)
-                
-                const showCustomers = `
-                <div id=table_${id}>
-                    <h1>Customer Name: ${store.attributes.name}</h1>
-                    <h3>Store Location: ${store.attributes.city}</h3>
-                    <h3>Store Phone: ${store.attributes.phone}</h3>
-                    <button class=store-info id=${store.id}> See ${store.attributes.name} Customer List </button>
-                    <button> Edit Store Information </button>
-                    <br>
-                    <br>
-                    <br>
-                </div>
-            `
-
-            })
-            // if (info.data.attributes.customers.length > 0) {
-            //     console.log(info.data.attributes.customers)
-            // } else {
-            //     alert("This store currently has no listed customers.");
-            // }
-            
-        })
-        
-    })
-    
 }
+
+storeContainer.addEventListener('click', function(e){
+    fetch(`http://localhost:3000/api/stores/${e.target.id}`)
+    .then(resp => resp.json())
+    .then(info => {
+        // console.log(info.data.attributes.customers.length)
+
+        info.data.attributes.customers.map(cust =>{
+            console.log(cust)
+            
+        })    
+    })   
+})
+    
 
 
  
