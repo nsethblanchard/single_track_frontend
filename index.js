114763c2c69c64de8dea95bf70ebb7e9f4972378
Seth Blanchard <nsethblanchard@gmail.com>
2021-05-12 13:12:14 -0400

    second fetch partial-parsing associated data

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const storeIndex = "http://localhost:3000/api/stores"
 
-document.addEventListener("DOMContentLoaded", function(){
-    // e.preventDefault()
-   getStoresIndex()
-});
+document.addEventListener("DOMContentLoaded", function(e){
+    e.preventDefault()
+    getStoresIndex()
+
+  
+
 
 function getStoresIndex(){
     fetch(storeIndex)
@@ -15,7 +17,7 @@ function getStoresIndex(){
                     <h1>Store Name: ${store.attributes.name}</h1>
                     <h3>Store Location: ${store.attributes.city}</h3>
                     <h3>Store Phone: ${store.attributes.phone}</h3>
-                    <button data-id=${store.id}> See ${store.attributes.name} Customers Information </button>
+                    <button class=store-info id=${store.id}> See ${store.attributes.name} Information </button>
                     <button> Edit Store Information </button>
                     <br>
                     <br>
@@ -25,4 +27,23 @@ function getStoresIndex(){
         document.querySelector('#store-container').innerHTML += showStore;    
         })
     })
+
+
+
+
+    const storeContainer = document.querySelector("#store-container")
+
+    storeContainer.addEventListener('click', function(e){
+
+        fetch(`http://localhost:3000/api/stores/${e.target.id}`)
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+        })
+        
+    })
+    
 }
+
+});
+ 
