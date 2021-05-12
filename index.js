986ce7d17e6739b093594c9e8096c8b17483f834
Seth Blanchard <nsethblanchard@gmail.com>
2021-05-12 10:41:12 -0400

    partial buildout of first fetch

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("we are in Index.js!");
+const storeIndex = "http://localhost:3000/api/stores"
+
+document.addEventListener("DOMContentLoaded", function(){
+    // e.preventDefault()
+   getStoresIndex()
+   
+    
+    
+
+});
+
+function getStoresIndex(){
+    fetch(storeIndex)
+    .then (resp => resp.json())
+    .then (stores => {
+        stores.data.forEach(store => {
+            const showStore = `
+                <div data-id=${store.id}>
+                    <h1>Store Name: ${store.attributes.name}</h1>
+                    <h3>Store Location: ${store.attributes.city}</h3>
+                    <h3>Store Phone: ${store.attributes.phone}</h3>
+            `
+            
+        })
+    
+    })
+}
