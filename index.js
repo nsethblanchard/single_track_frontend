d4bcf79a5321a4e58540658b8becc91926f97f2f
Seth Blanchard <nsethblanchard@gmail.com>
2021-05-13 07:10:11 -0400

    changing js file for different looks

--- index.js
+++ index.js
@@ -13,14 +13,10 @@ function getStoresIndex(){
         stores.data.map(store => {
             const showStore = `
                 <div id=${store.id}>
-                    <h1>Store Name: ${store.attributes.name}</h1>
-                    <h3>Store Location: ${store.attributes.city}</h3>
-                    <h3>Store Phone: ${store.attributes.phone}</h3>
-                    <button class=store-info id=${store.id}> See ${store.attributes.name} Customer List </button>
+                    <h4>Store Name: ${store.attributes.name}</h4>
+                    <button class=store-info id=${store.id}> See ${store.attributes.name} Information </button>
                     <button> Edit Store Information </button>
-                    <br>
-                    <br>
-                    <br>
+                    <br>    
                 </div>`
         document.querySelector('#store-container').innerHTML += showStore;    
         })
