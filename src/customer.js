aeedd52f2688d5f075876dafa814a95ecaa66413
Seth Blanchard <nsethblanchard@gmail.com>
2021-05-14 20:19:31 -0400

    store js/class

--- src/customer.js
+++ src/customer.js
@@ -1,7 +1,11 @@
 
 
 class Customer {
-
+    constructor(name, email, phone, stravaURL, bikeStyle){
+        this.id = 
+        this.name = name;
+        this.email = 
+    }
 
 
     
---
+++ src/store.js
@@ -0,0 +1,12 @@
+
+
+
+class Store {
+    constructor() {
+
+        
+    }
+
+
+
+}
