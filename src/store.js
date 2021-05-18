2b9d26a001079ede4569fae6af90af5f3b26a11f
Seth Blanchard <nsethblanchard@gmail.com>
2021-05-18 16:15:27 -0400

    stores index and create store form made

--- src/store.js
+++ src/store.js
@@ -2,11 +2,12 @@
 
 
 class Store {
-    constructor(id, attributes) {
-        this.id = id;
-        this.name = attributes.name;
-        this.phone = attributes.phone;
-        this.city = attributes.city;
+    constructor(store) {
+        
+        this.id = store.id;
+        this.name = store.attributes.name;
+        this.phone = store.attributes.phone;
+        this.city = store.attributes.city;
         Store.all.push(this);
         // debugger
     }
@@ -15,10 +16,11 @@ class Store {
     renderStore() {
         // debugger
         return `
-            <div data-id=${this.id}>
+            <div data-id="${this.id}">
                 <h1>${this.name}</h1>
                 <h3>Located in ${this.city}</h3>
                 <h3>${this.phone}</h3>
+                <button type="button" id="${this.id}" class="btn-secondary delete-store"> Delete ${this.name} </button>
             </div>
             <br>
         `
