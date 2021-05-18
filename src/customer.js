bf35dd72e3fd7170d266bd7e6ae1b317b11a550c
Seth Blanchard <nsethblanchard@gmail.com>
2021-05-18 15:43:14 -0400

    initial commit for new branch

--- src/customer.js
+++ src/customer.js
@@ -14,7 +14,6 @@ class Customer {
             <div class="float-child">
                 <h5>${this.name}</h5>
                 <p>${this.phone}</p>
-                <p>${this.email}</p>
                 <p>Customer Bike Style: ${this.bikeStyle}</p>
                 <button type="button"  class="btn-secondary edit-customer"> Edit ${this.name}</button>
                 <button type="button" id="${this.id}" class="btn-secondary delete-customer"> Delete ${this.name} </button>
